b393869669fa2a11fee98ecf204bea52000ccf1c
Saydullaev <s.khabibullo@pegb.tech>
2024-10-07 12:45:42 +0500

    sdf

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import './App.css';
 import PrivacyPolicy from './Privacy';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
-  useEffect(() => {
-    if (window.location.pathname !== '/') {
-      return (window.location = '/');
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (window.location.pathname !== '/') {
+  //     return (window.location = '/');
+  //   }
+  // }, []);
 
   return (
-    <div className='App'>
-      <PrivacyPolicy />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/privacy-policy'>Privacy Policy</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/privacy-policy' component={PrivacyPolicy} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
